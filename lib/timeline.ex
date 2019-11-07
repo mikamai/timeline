862f86919839e093971dc1c34e0e8f6e8b441191
Oscar Bernardini <bernardini687@gmail.com>
2019-11-07 11:00:04 +0100

    code entry point

--- lib/timeline.ex
+++ lib/timeline.ex
@@ -3,16 +3,37 @@ defmodule Timeline do
   Documentation for Timeline.
   """
 
-  @doc """
-  Hello world.
+  @help Application.get_env(:timeline, :main_help)
 
-  ## Examples
+  alias Timeline.{Entries, Projects}
 
-      iex> Timeline.hello()
-      :world
+  def main([]) do
+    display_help()
+  end
 
-  """
-  def hello do
-    :world
+  def main(args) do
+    [resource | args] = args
+    dispatch_resource(resource, args)
+  end
+
+  defp dispatch_resource(resource, []) do
+    resource = "#{__MODULE__}.#{String.capitalize(resource)}"
+    String.to_atom(resource).display_help()
+  end
+
+  defp dispatch_resource("entries", args) do
+    Entries.run(args)
+  end
+
+  defp dispatch_resource("projects", args) do
+    Projects.run(args)
+  end
+
+  defp dispatch_resource(_, _) do
+    display_help()
+  end
+
+  defp display_help do
+    IO.write(@help)
   end
 end
