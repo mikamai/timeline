f25f2e4d1159aabc694348eef8a65d0d17695de3
Oscar Bernardini <bernardini687@gmail.com>
2019-11-08 12:54:34 +0100

    format support

--- lib/support/support.ex
+++ lib/support/support.ex
@@ -1,11 +1,11 @@
 defmodule Support do
-  # can i have @resources here and access it elsewhere?
   @resources ~w(entries projects)
 
   def get_resources, do: @resources
 
   def display_help_for(resource) when resource in @resources do
     resource_help = resource <> "_help"
+
     Application.get_env(:timeline, String.to_atom(resource_help))
     |> IO.write()
   end
