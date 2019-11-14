c2d13e49dfcf6c1dfd9aed2e1e48bf66d490bc00
Oscar Bernardini <bernardini687@gmail.com>
2019-11-14 11:40:39 +0100

    scaffold entries add

--- lib/timeline/entries/add.ex
+++ lib/timeline/entries/add.ex
@@ -1,28 +1,70 @@
 defmodule Timeline.Entries.Add do
+  @options [
+    aliases: [d: :date, n: :notes, a: :amount],
+    strict: [date: :string, notes: :string, amount: :integer]
+  ]
+
   def run(args) do
-    parse_args(args)
+    args
+    |> parse_args()
+    |> process()
   end
 
   def parse_args(args) do
-    parsed =
-      OptionParser.parse(
-        args,
-        aliases: [
-          d: :date,
-          p: :project,
-          t: :task,
-          n: :notes,
-          h: :hours
-        ],
-        strict: [
-          date: :string,
-          project: :string,
-          task: :string,
-          notes: :string,
-          hours: :integer
-        ]
-      )
+    parsed = OptionParser.parse(args, @options)
 
-    IO.inspect(parsed)
+    case parsed do
+      {[amount: amount], [project_task], []} ->
+        [:default, :default, amount] ++ split(project_task)
+
+      _ ->
+        :help
+    end
+  end
+
+  def process(values) when length(values) < 5 do
+    IO.puts("Usage: timeline entries add <project>:<task> [OPTIONS]")
+  end
+
+  def process([:default, :default | values] = _) do
+    keys = ~w(executed_on notes amount project task)a
+    values = [today(), "" | values]
+
+    bodify([keys, values])
+    |> IO.puts()
+  end
+
+  defp split(project_task) do
+    project_task = String.split(project_task, ":")
+
+    case length(project_task) do
+      2 ->
+        project_task
+
+      _ ->
+        [:error]
+    end
+  end
+
+  defp bodify(key_value_pairs) do
+    encoded =
+      key_value_pairs
+      |> List.zip()
+      |> Enum.into(%{})
+      |> Jason.encode()
+
+    case encoded do
+      {:ok, body } ->
+        body
+
+      _ ->
+        IO.puts("Usage: timeline entries add <project>:<task> [OPTIONS]")
+    end
+  end
+
+  defp today do
+    # YYYY/MM/DD
+    Date.utc_today()
+    |> Date.to_string()
   end
 end
